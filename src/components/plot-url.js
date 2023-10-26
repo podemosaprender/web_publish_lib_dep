b16cc6607131bfc91987651b0ce3b74f3e0aaeb0
Stradex <stradexengine@gmail.com>
2023-10-26 12:43:05 -0300

    OK: Comentando dos console log

--- src/components/plot-url.js
+++ src/components/plot-url.js
@@ -5,8 +5,8 @@ export const PlotUrl= async ({url, opts, dataFun, layout}) => {
 	let dsrc= (opts?.fmt=='csv' || opts?.delimiter!=null || url.endsWith('.csv')) 
 		? await fetch_csv(url, {...opts, colsAsArray: true})
 		: await fetch_json(url);
-	console.log("PlotUrl dsrc", dsrc)
+	//DBG: console.log("PlotUrl dsrc", dsrc)
 	let data= (typeof(dataFun)=='function') ? dataFun(dsrc, opts) : dsrc;
-	console.log("PlotUrl data", data)
+	//DBG: console.log("PlotUrl data", data)
 	return (<Plot data={data} layout={layout} />)
 }
