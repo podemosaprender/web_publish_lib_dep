0dc1902fb7e15774af1c8021b1632461194054c2
Stradex <stradexengine@gmail.com>
2023-10-31 21:47:41 -0300

    OK: to-next.mjs now also works with md and mdx

--- src/lib/to-next.mjs
+++ src/lib/to-next.mjs
@@ -14,8 +14,8 @@ import path from 'path'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const IPYNB_PAGE_TPL = fs.readFileSync(__dirname + "/../_ipynb_template.js");
 
-let SRC_DIR = "c:/Users/Maira/Desktop/Maxi/Trabajo/PodemosAprender/Sitio/publish_on_gitpages_tpl" //XXX: Solo para test
-let DST_DIR = "./xnext" //U: Source files for Next //XXX: Solo para test
+let SRC_DIR = "../publish_on_gitpages_tpl" //XXX: Solo para test
+let DST_DIR = "./src/app" //U: Source files for Next //XXX: Solo para test
 
 async function mkDir(apath) {
     fs.mkdirSync(DST_DIR + "/" + apath, { recursive: true });
@@ -27,17 +27,21 @@ async function ipynbToHtml(apath, dst_folder) {
     .replace("$src_file", src_file);
 
     console.log("ipynbToHtml", cmd);
-    //execSync(cmd);
+    execSync(cmd);
 }
-async function ipynbPageTemplate(carpeta) {
-    fs.writeFileSync(DST_DIR + "/" + carpeta + "/page.js", IPYNB_PAGE_TPL);
+async function ipynbPageTemplate(dst_folder) {
+    fs.writeFileSync(DST_DIR + "/" + dst_folder + "/page.js", IPYNB_PAGE_TPL);
+}
+
+async function markdownCopyFile(apath, dst_folder) { 
+    fs.writeFileSync(DST_DIR + "/" + dst_folder + "/page.mdx", fs.readFileSync(SRC_DIR + "/" + apath));
 }
 
 async function processEntry(apath) {
     let src_path = SRC_DIR + "/" + apath;
     if (fs.statSync(src_path).isDirectory()) {
         mkDir(apath);
-    } else {
+      } else {
         if (apath.endsWith(".ipynb")) {
             let carpeta = apath.endsWith("page.ipynb")
                 ? apath.replace("page.ipynb", "")
@@ -46,6 +50,14 @@ async function processEntry(apath) {
             mkDir(carpeta);
             ipynbToHtml(apath, carpeta);
             ipynbPageTemplate(carpeta);
+        } else if (apath.endsWith(".md") || apath.endsWith(".mdx")) { 
+            let file_ext = apath.endsWith(".md") ? '.md' : '.mdx';
+            let carpeta = apath.endsWith("page" + file_ext)
+                ? apath.replace("page" + file_ext, "")
+                : apath.replace(file_ext, ""); 
+            console.log("Procesando mdx", {apath, carpeta});
+            mkDir(carpeta);
+            markdownCopyFile(apath, carpeta);
         }
     }
 }
@@ -53,8 +65,8 @@ async function processEntry(apath) {
 async function main() {
     let userfiles = (await files(SRC_DIR, "*")) //A: Must filter later.
         .map(fn => fn.slice(SRC_DIR.length+1)) //A: Remove prefix
-        .filter(fn => fn.indexOf("_build") == -1)
-    //DBG: console.log({userfiles});
+        .filter(fn => fn.indexOf("_build") == -1 && fn.indexOf("README.md") == -1)
+    console.log({userfiles});
     userfiles.forEach( processEntry );
 }
 
