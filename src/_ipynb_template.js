800d07349757bd3bac1bc94223280d1cecfc0f60
Stradex <stradexengine@gmail.com>
2023-11-01 19:42:11 -0300

    OK: Generate IPYNB metadata

--- src/_ipynb_template.js
+++ src/_ipynb_template.js
@@ -9,6 +9,17 @@ async function getJupyterSiteSrc(apath) {
 	return siteSrc_;
 }
 
+export async function generateMetadata() {
+	let htmlSrc = await getJupyterSiteSrc(__dirname+'/jupyter.html');
+	let siteTitle = (htmlSrc.match(/<h1.*?>(.*?)(<a.*?<\/a>)?<\/h1>/)||[])[1] || (htmlSrc.match(/<title>([^<]*)/)||[])[1];
+	return {
+	  title: siteTitle,
+	  other: {
+		blog_title: siteTitle
+	  }
+	};
+};
+
 export default async function Page() {
 	let src= await getJupyterSiteSrc(__dirname+'/jupyter.html')
 	return (<div
