de26efa794cc595d55250a47fc3398135f8073c0
Stradex <stradexengine@gmail.com>
2023-10-31 21:48:18 -0300

    TEST: Adding default metadata to IPYNB Template

--- src/_ipynb_template.js
+++ src/_ipynb_template.js
@@ -1,5 +1,15 @@
 import {fetch_txt} from '@/lib/fetch-data'
 
+export const metadata = {
+	title: "Google Collab Post",
+	description: "Google Collab Post",
+	other: {
+	  author: "Post Author",
+	  date: "31-10-2023",
+	  blog_title: "Google Collab Post",
+	}
+};
+
 export default async function Page() {
 	let src= await fetch_txt(__dirname+'/jupyter.html')
 	return (<div
