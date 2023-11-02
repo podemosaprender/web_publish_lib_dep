c636eb258603aea2c234972c608b7a7fe50a0a8f
Stradex <stradexengine@gmail.com>
2023-11-02 10:34:46 -0300

    OK: Making colab responsive for mobile

--- src/lib/parse-data.mjs
+++ src/lib/parse-data.mjs
@@ -1,4 +1,4 @@
-//XXX: Why are we getting &amp characters in site-map.txt?
+//XXX: Why are we getting ASCII characters in jupyter.html?
 export function ASCIItoUTF8(asciiString, pattStr='') {
     let rePattern = new RegExp('&' + pattStr + '#(\\d+);', 'g');
 	return asciiString.replace(rePattern, function(match, number) {
