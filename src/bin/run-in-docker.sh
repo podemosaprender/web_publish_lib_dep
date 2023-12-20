ce75dbcbbcdea9fa2ee8b9eea0cd117cbb3568e9
devel@mauriciocap.com <devel@mauriciocap.com>
2023-12-20 14:03:12 +0000

    FIX: run-in-docker windows gitbash/cygwin

--- src/bin/run-in-docker.sh
+++ src/bin/run-in-docker.sh
@@ -3,6 +3,10 @@
 
 APT_CACHE="`pwd`/x_run/apt"
 
-docker run -it -v `pwd`:/app -w /app -v $APT_CACHE:/var/cache/apt ubuntu:22.04 /bin/bash -c './site/src/bin/setup-ubuntu.sh; $SHELL'
+if pwd --help | grep -- -W ; then docker_pwd=`pwd -W` ; fi
+if [ -z "$docker_pwd" ] && which cygpath ; then docker_pwd=`cygpath -w -a .`; fi
+if [ -z "$docker_pwd" ]; then docker_pwd=`pwd`; fi
+
+docker run -it -v "$docker_pwd":/app -w /app -v $APT_CACHE:/var/cache/apt ubuntu:22.04 /bin/bash -c './site/src/bin/setup-ubuntu.sh; $SHELL'
 
 
